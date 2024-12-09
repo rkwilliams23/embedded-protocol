99a63e9c03e14ec13bb9d06e72c9ae5dab394b72
R.K.WILLIAMS <rk.williams23@icloud.com>
2024-12-09 14:19:35 +0530

    unknown.c

---
+++ UART/unknown.c
@@ -0,0 +1,29 @@
+class transaction;
+
+typedef enum bit
+
+randc oper_type op;
+
+bit rx;
+
+rand bit [7:0] dir
+
+bit newd;
+bit tx;
+
+bit [7:0] doutrx;
+bit donetx;
+bit donerx;
+
+
+
+function transaction
+copy = new();
+copy.rx = this.r
+  copy.dintx = this
+  copy.newd = this
+  copy.tx = this.t
+  copy.doutrx = th
+  copy.donetx = th
+  copy.donerx = th
+  copy.oper = this
